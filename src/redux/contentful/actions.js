df8040368213b5e15cbf8e7857dc9808b469c4db
Emmanuel Temilola Ogunbo <Ogunbotemilola@yahoo.com>
2019-12-03 15:00:02 -0800

    format file (#10)

--- src/redux/contentful/actions.js
+++ src/redux/contentful/actions.js
@@ -1,32 +1,27 @@
 import { ContentfulActionTypes } from "./types";
 import { getEntries } from "../../helpers/contentful";
 
-export const fetchContentfulStart = () => (
-    {type: ContentfulActionTypes.FETCH_CONTENTFUL_START}
-);
+export const fetchContentfulStart = () => ({
+  type: ContentfulActionTypes.FETCH_CONTENTFUL_START
+});
 
-export const fetchContentfulStartAsync = (slug) => async dispatch =>{
-    dispatch(fetchContentfulStart());
-    try {
-        const entries = await getEntries(slug);
-        dispatch(fetchContentfulSucess(entries));
-    } catch (error) {
-        console.error(error)
-        dispatch (fetchContentfulError(error)); 
-    }
-}
+export const fetchContentfulStartAsync = slug => async dispatch => {
+  dispatch(fetchContentfulStart());
+  try {
+    const entries = await getEntries(slug);
+    dispatch(fetchContentfulSucess(entries));
+  } catch (error) {
+    console.error(error);
+    dispatch(fetchContentfulError(error));
+  }
+};
 
+export const fetchContentfulSucess = payload => ({
+  type: ContentfulActionTypes.FETCH_CONTENTFUL_SUCCESS,
+  payload: payload
+});
 
-export const fetchContentfulSucess = (payload) => (
-    {
-        type: ContentfulActionTypes.FETCH_CONTENTFUL_SUCCESS, 
-        payload: payload
-    }
-);
-
-export const fetchContentfulError = (error) => (
-    {
-        type: ContentfulActionTypes.FETCH_CONTENTFUL_ERROR, 
-        payload: error
-    }
-);
+export const fetchContentfulError = error => ({
+  type: ContentfulActionTypes.FETCH_CONTENTFUL_ERROR,
+  payload: error
+});
