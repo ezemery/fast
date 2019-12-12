082a049dbc9601956e160eb5d55456b332bddb9a
Eze <emereuwaonueze@gmail.com>
2019-12-12 20:25:59 +0100

    update node engine

--- src/components/brand/index.js
+++ src/components/brand/index.js
@@ -1,27 +1,9 @@
-<<<<<<< HEAD
-import React from 'react';
-import Swiper from 'react-id-swiper';
-import AspectRatio from 'react-aspect-ratio';
-import 'react-aspect-ratio/aspect-ratio.css';
-import 'react-id-swiper/lib/styles/css/swiper.css';
-import './style.scss';
-import 'swiper/css/swiper.css';
-import HarrisFarmImg from '../../assets/img/harris_farm.png';
-import Hundred24Img from '../../assets/img/24hundred.png';
-import AmazonImg from '../../assets/img/amazon.png';
-import FashionNovaImg from '../../assets/img/fashion_nova.png';
-import HarrisFarmBackImg from '../../assets/img/harris_farm_back.png';
-import Hundred24BackImg from '../../assets/img/24hundred_back.png';
-import AmazonBackImg from '../../assets/img/amazon_back.png';
-import FashionNovaBackImg from '../../assets/img/fashion_nova_back.png';
-=======
 import React from "react";
 import Swiper from "react-id-swiper";
 import AspectRatio from "react-aspect-ratio";
 import "react-aspect-ratio/aspect-ratio.css";
 import "react-id-swiper/lib/styles/css/swiper.css";
 import "./style.scss";
->>>>>>> develop
 
 function BrandItem(props) {
   const { name, industry, backImg } = props;
