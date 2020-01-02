43ba65c341439c99ccb4127943e397d36972d7ee
Eze <emereuwaonueze@gmail.com>
2020-01-02 16:43:55 +0100

    fix container port issue

--- src/pages/error.js
+++ src/pages/error.js
@@ -3,7 +3,7 @@ import NotFound from '../components/404';
 import ServerError from '../components/500';
 
 function Error({ statusCode }) {
-    { statusCode == 404 ? <NotFound/> : <ServerError/>}
+    { statusCode == 404 ?  <NotFound/> : <ServerError/>}
 }
 
 Error.getInitialProps = ({ res, err }) => {
