87a59fad33a76b4bce600891265b7f955d5a95a5
Eze <emereuwaonueze@gmail.com>
2020-01-21 12:48:04 +0100

    add image optimization

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 const withSass = require('@zeit/next-sass')
 const withCSS = require("@zeit/next-css");
 const withLess = require('@zeit/next-less');
+const withOptimizedImages = require('next-optimized-images');
 require('dotenv').config()
 const webpack = require('webpack');
 
 //const withBundleAnalyzer = require('@next/bundle-analyzer');
-module.exports = withLess(withSass(withCSS({
+module.exports = withLess(withSass(withCSS(withOptimizedImages({
     env: {
         REACT_APP_CONTENTFUL_API_KEY: process.env.REACT_APP_CONTENTFUL_API_KEY,
         REACT_APP_ACCESS_TOKEN: process.env.REACT_APP_ACCESS_TOKEN,
@@ -40,4 +41,4 @@ module.exports = withLess(withSass(withCSS({
         return config
       }
       
-})));
+}))));
