44cfc4d5f892a9d81111a342e5c07b4b28293f89
Eze <emereuwaonueze@gmail.com>
2020-01-22 17:55:52 +0100

    add eslint + prettier

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
-const withSass = require('@zeit/next-sass')
-const withCSS = require("@zeit/next-css");
+const withSass = require('@zeit/next-sass');
+const withCSS = require('@zeit/next-css');
 const withLess = require('@zeit/next-less');
-require('dotenv').config()
+require('dotenv').config();
 const webpack = require('webpack');
 
 //const withBundleAnalyzer = require('@next/bundle-analyzer');
-module.exports = withLess(withSass(withCSS({
-    env: {
+module.exports = withLess(
+  withSass(
+    withCSS({
+      env: {
         REACT_APP_CONTENTFUL_API_KEY: process.env.REACT_APP_CONTENTFUL_API_KEY,
         REACT_APP_ACCESS_TOKEN: process.env.REACT_APP_ACCESS_TOKEN,
-        REACT_APP_PREVIEW_ACCESS_TOKEN: process.env.REACT_APP_PREVIEW_ACCESS_TOKEN,
+        REACT_APP_PREVIEW_ACCESS_TOKEN:
+          process.env.REACT_APP_PREVIEW_ACCESS_TOKEN,
         REACT_APP_SPACE_ID: process.env.REACT_APP_SPACE_ID,
       },
       distDir: 'build',
@@ -19,17 +22,18 @@ module.exports = withLess(withSass(withCSS({
 
       webpack(config, { dev }) {
         config.module.rules.push({
-            test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-            use: {
-              loader: 'url-loader',
-              options: {
-                limit: 100000,
-                name: '[name].[ext]'
-              }
-            }
-          })
+          test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 100000,
+              name: '[name].[ext]',
+            },
+          },
+        });
 
-        return config
-      }
-      
-})));
+        return config;
+      },
+    })
+  )
+);
